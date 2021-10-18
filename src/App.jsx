946c56024f9fea4de5d1342d8651c16c509a0990
cesar diaz <arq.cmdiaz1@gmail.com>
2021-10-18 13:36:46 -0500

    Click example

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,27 @@
+import * as THREE from 'three'
+import { useRef, useState } from "react"
+import { Canvas, useFrame } from "@react-three/fiber"
+
 export default function App() {
-  return "hello"
+
+  function Box() {
+    const ref = useRef()
+    const [clicked, setClicked] = useState(false)
+    useFrame((state) => {
+      ref.current.position.z = THREE.MathUtils.lerp(ref.current.position.z, clicked ? 2 : 0, 0.1)
+    })
+
+    return (
+      <mesh ref={ref} onClick={() => setClicked(!clicked)}>
+      <boxGeometry />
+      <meshBasicMaterial color='orange' />
+    </mesh>
+    )
+  }
+
+  return (
+  <Canvas>
+    <Box />
+  </Canvas>
+  )
 }
